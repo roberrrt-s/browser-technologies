5dc1551ddddcd1ea3f86a4b88aa5f56e7f3a13a6
roberrrt-s <robert.spier@gmail.com>
2016-04-01 21:54:46 +0200

    cleaned up html/css/js (semantic html)
    Yes, Krijn, I have created some semantic HTML!

--- js/global.js
+++ js/global.js
@@ -1,35 +1,13 @@
 (function() {
 
-	var remove = {
-		init: function() {
-
-			var inv = document.querySelectorAll('.invisible')
-
-			for(var i = 0; i < inv.length; i++) {
-				inv[i].setAttribute('class', 'alpha')
-			}
-
-			var fallback = document.querySelectorAll('.fallback')
-
-			var button = document.querySelector('button')
-
-			button.parentNode.removeChild(button);
-
-			console.log(fallback.length)
-
-			for(var i = 0; i < fallback.length; i++) {
-				fallback[i].parentNode.removeChild(fallback[i]);
-			}
-
-		}
-	}
-
 	var drag = {
 
 		init: function() {
 
 			var self = this;
 			var alpha = document.getElementsByClassName('alpha');
+
+			console.log(alpha)
 
 			for(var i = 0; i < alpha.length; i++) {
 				alpha[i].addEventListener('dragstart', self.start, false);
@@ -96,38 +74,11 @@
 
 			for(var i = 0; i < alpha.length; i++) {
 				alpha[i].addEventListener('click', self.element, false);
-				alpha[i].addEventListener('onkeydown', self.tab, false);
 			}
 		},
+
 
 		element: function(e) {
-
-			if(click.src) {
-
-				var parent = click.src.parentNode
-
-				e.target.parentNode.insertBefore(click.src, e.target)
-				parent.appendChild(e.target)
-
-				click.src = null;
-
-			}
-			else {
-				if(e.target.getAttribute('draggable') == "true") {
-					click.src = e.target
-				}
-			}
-
-		},
-
-		tab: function(e) {
-
-			console.log("HELLO")
-
-			if(!e.keyCode !== 13) {
-				console.log('not enter');
-				return false
-			}
 
 			if(click.src) {
 
@@ -150,7 +101,6 @@
 		src: null
 	}
 
-	remove.init();
 	drag.init();
 	click.init();
 
